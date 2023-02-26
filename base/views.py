c953fe1014f0fa068bca702e9ac85f8bbc6a1068
Wiklyn Craveiro <wiklynsc@gmail.com>
2023-02-26 10:28:04 -0300

    during user registration

--- base/views.py
+++ base/views.py
@@ -4,6 +4,7 @@ from django.db.models import Q
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from .models import Room, Topic
 from .forms import RoomForm
@@ -19,7 +20,7 @@ from .forms import RoomForm
 
 
 def loginPage(request):
-
+  page = 'login'
   if request.user.is_authenticated:
     return redirect('home')
 
@@ -40,13 +41,27 @@ def loginPage(request):
     else:
       messages.error(request, 'Username or password does not exist.')
 
-  context = {}
+  context = {'page': page}
   return render(request, 'base/login_register.html', context)
 
 
 def logoutUser(request):
   logout(request)
   return redirect('home')
+
+
+def registerPage(request):
+  form = UserCreationForm()
+
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save(commit=False)
+      user.username = user.username.lower()
+      user.save()
+
+  context = {'form': form}
+  return render(request, 'base/login_register.html', context)
 
 
 def home(request):
