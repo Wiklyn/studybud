90d6a5c0d7013873eff940d8498002f3eadc868b
Wiklyn Craveiro <wiklynsc@gmail.com>
2023-02-26 09:52:08 -0300

    before user registrarion

--- base/views.py
+++ base/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from django.db.models import Q
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from .models import Room, Topic
 from .forms import RoomForm
 
@@ -17,6 +19,9 @@ from .forms import RoomForm
 
 
 def loginPage(request):
+
+  if request.user.is_authenticated:
+    return redirect('home')
 
   if request.method == 'POST':
     username = request.POST.get('username')
@@ -33,10 +38,15 @@ def loginPage(request):
       login(request, user)
       return redirect('home')
     else:
-      messages.error('Username or password does not exist.')
+      messages.error(request, 'Username or password does not exist.')
 
   context = {}
   return render(request, 'base/login_register.html', context)
+
+
+def logoutUser(request):
+  logout(request)
+  return redirect('home')
 
 
 def home(request):
@@ -58,6 +68,7 @@ def room(request, pk):
   return render(request, 'base/room.html', context)
 
 
+@login_required(login_url='login')
 def createRoom(request):
   form = RoomForm()
 
@@ -71,9 +82,13 @@ def createRoom(request):
   return render(request, 'base/room_form.html', context)
 
 
+@login_required(login_url='login')
 def updateRoom(request, pk):
   room = Room.objects.get(id=pk)
   form = RoomForm(instance=room)
+
+  if request.user != room.host:
+    return HttpResponse('You are not allowed here!')
 
   if request.method == 'POST':
     form = RoomForm(request.POST, instance=room)
@@ -85,8 +100,12 @@ def updateRoom(request, pk):
   return render(request, 'base/room_form.html',  context)
 
 
+@login_required(login_url='login')
 def deleteRoom(request, pk):
   room = Room.objects.get(id=pk)
+
+  if request.user != room.host:
+    return HttpResponse('You are not allowed here!')
 
   if request.method == 'POST':
     room.delete()
